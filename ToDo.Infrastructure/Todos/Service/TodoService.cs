b5f1a218ac88fe3412db77ddd09905ed7e2d0162
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-23 22:48:58 +0500

    Added Service and validator

--- ToDo.Infrastructure/Todos/Service/TodoService.cs
+++ ToDo.Infrastructure/Todos/Service/TodoService.cs
@@ -1,6 +1,56 @@
-﻿namespace ToDo.Infrastructure.Todos.Service;
+﻿using System.Linq.Expressions;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using ToDo.Application.Todos.Services;
+using ToDo.Domain.Entities;
+using ToDo.Persistence.Repostiries.Interfaces;
+using ValidationException = System.ComponentModel.DataAnnotations.ValidationException;
 
-public class TodoService
+namespace ToDo.Infrastructure.Todos.Service;
+
+public class TodoService(ITodoRepository todoRepository, IValidator<TodoItem> todoValidator) : ITodoService
 {
+    public IQueryable<TodoItem> Get(Expression<Func<TodoItem, bool>>? predicate = default, bool asNoTracking = false)
+    {
+        return todoRepository.Get(predicate, asNoTracking);
+    }
     
+    public async ValueTask<IList<TodoItem>> GetAsync(bool asNoTracking = false)
+    {
+        var todos = await todoRepository.Get().ToListAsync();
+
+        return todos 
+            .Where(todo => !todo.IsDone && todo.DueTime > DateTime.Now).OrderBy(todo => todo.DueTime)
+            .Concat(todos.Where(todo => todo.IsDone).OrderByDescending(todo => todo.ModifiedTime))
+            .Concat(todos.Where(todo => todo.IsDone && todo.DueTime <= DateTime.Now)).OrderByDescending(todo => todo.DueTime)
+            .ToList();
+    }
+
+    public ValueTask<TodoItem?> GetByIdAsync(Guid todoId, bool asNoTracking = false, CancellationToken cancellationToken = default)
+    {
+        return todoRepository.GetByIdAsync(todoId, asNoTracking, cancellationToken);
+    }
+
+    public ValueTask<TodoItem> CreateAsync(TodoItem todoItem, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        var validationResult = todoValidator.Validate(todoItem);
+        
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        todoItem.CreatedTime = DateTimeOffset.UtcNow;
+
+        return todoRepository.CreateAsync(todoItem, saveChanges, cancellationToken);
+    }
+
+    public ValueTask<bool> UpDateAsync(TodoItem todoItem, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> DeleteByIdAsync(Guid todoId, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
 }
--- ToDo.Infrastructure/Validators/TodoValidator.cs
+++ ToDo.Infrastructure/Validators/TodoValidator.cs
@@ -1,6 +1,33 @@
-﻿namespace ToDo.Infrastructure.Validators;
+﻿using FluentValidation;
+using ToDo.Domain.Entities;
+using ToDo.Domain.Enums;
 
-public class TodoValidator
+namespace ToDo.Infrastructure.Validators;
+
+public class TodoValidator : AbstractValidator<TodoItem>
 {
-    
+    public TodoValidator()
+    {
+        RuleSet(
+            EntityEvent.OnCreate.ToString(),
+            () =>
+            {
+                RuleFor(todo => todo.Title).NotEmpty().MinimumLength(3).MaximumLength(128);
+                RuleFor(todo => todo.IsDone).Equal(false);
+                RuleFor(todo => todo.IsFavorite).Equal(false);
+                RuleFor(todo => todo.DueTime).GreaterThan(DateTimeOffset.Now);
+                RuleFor(todo => todo.ReminderTime).GreaterThan(DateTimeOffset.Now);
+            }
+        );
+        
+        RuleSet(
+            EntityEvent.OnUpdate.ToString(),
+            () =>
+            {
+                RuleFor(todo => todo.Title).NotEmpty().MinimumLength(3).MaximumLength(128);
+                RuleFor(todo => todo.DueTime).GreaterThan(DateTimeOffset.Now);
+                RuleFor(todo => todo.ReminderTime).GreaterThan(DateTimeOffset.Now);
+            }
+        );
+    }
 }
