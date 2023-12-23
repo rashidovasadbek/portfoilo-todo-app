a670b895dcb209a87c432959f45a7a0edc3fa07b
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-23 22:47:57 +0500

    Added Service

--- ToDo.Application/Todos/Services/ITodoService.cs
+++ ToDo.Application/Todos/Services/ITodoService.cs
@@ -1,6 +1,18 @@
-﻿namespace ToDo.Application.Todos.Services;
+﻿using ToDo.Domain.Entities;
+
+namespace ToDo.Application.Todos.Services;
 
 public interface ITodoService
 {
-    
+    ValueTask<IList<TodoItem>> GetAsync(bool asNoTracking = false);
+
+    ValueTask<TodoItem?> GetByIdAsync(Guid todoId, bool asNoTracking = false,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<TodoItem> CreateAsync(TodoItem todoItem, bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<bool> UpDateAsync(TodoItem todoItem, CancellationToken cancellationToken = default);
+
+    ValueTask<bool> DeleteByIdAsync(Guid todoId, CancellationToken cancellationToken = default);
 }
