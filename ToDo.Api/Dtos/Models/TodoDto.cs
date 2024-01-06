fde2fc23753e89760f2fdc0b6f774d97973c7648
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-01-06 10:09:21 +0500

    update Dto

--- ToDo.Api/Dtos/Models/TodoDto.cs
+++ ToDo.Api/Dtos/Models/TodoDto.cs
@@ -2,5 +2,15 @@
 
 public class TodoDto
 {
-    
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = default!;
+
+    public bool IsDone { get; set; }
+
+    public bool IsFavorite { get; set; }
+
+    public DateTimeOffset DueTime { get; set; }
+
+    public DateTimeOffset ReminderTime { get; set; }
 }
