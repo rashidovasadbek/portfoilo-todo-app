8d0eafd89fd7b6f4d8c3d65cf5f3f51708d5e5e0
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-01-06 10:08:45 +0500

    Added Host  Configurations

--- ToDo.Api/Configurations/HostConfiguration.Extensions.cs
+++ ToDo.Api/Configurations/HostConfiguration.Extensions.cs
@@ -1,19 +1,51 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using ToDo.Application.Todos.Services;
+using ToDo.Infrastructure.Todos.Service;
 using ToDo.Persistence.DataContext;
+using ToDo.Persistence.Repostiries;
+using ToDo.Persistence.Repostiries.Interfaces;
 
 namespace ToDo.Api.Configurations;
 
 public static partial class HostConfiguration
 {
+    private static readonly ICollection<Assembly> Assemblies;
 
+    static HostConfiguration()
+    {
+        Assemblies = Assembly.GetExecutingAssembly().GetReferencedAssemblies().Select(Assembly.Load).ToList();
+        Assemblies.Add(Assembly.GetExecutingAssembly());
+    }
+
+    private static WebApplicationBuilder AddValidators(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddValidatorsFromAssemblies(Assemblies);
+        
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddMappers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAutoMapper(Assemblies);
+        
+        return builder;
+    }
+   
     private static WebApplicationBuilder AddBusinessLogicInfrastructure(this WebApplicationBuilder builder)
     {
         //persistace
         builder.Services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(builder.Configuration.GetConnectionString("AppDatabaseConnection")));
-
+        // repository
+        builder.Services.AddScoped<ITodoRepository, TodoRepository>();
+        
+        //service
+        builder.Services.AddScoped<ITodoService, TodoService>();
         return builder;
     }
+   
     private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
     {
         builder.Services.AddRouting(options => options.LowercaseUrls = true);
@@ -30,6 +62,25 @@ public static partial class HostConfiguration
         return builder;
     }
 
+    private static WebApplicationBuilder AddCors(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddCors(
+            options =>
+            {
+                options.AddDefaultPolicy(policyBuilder =>
+                {
+                    policyBuilder
+                        .AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+                
+            }
+        );
+        
+        return builder;
+    }
+    
     private static WebApplication UseExposers(this WebApplication app)
     {
         app.MapControllers();
--- ToDo.Api/Configurations/HostConfiguration.cs
+++ ToDo.Api/Configurations/HostConfiguration.cs
@@ -4,13 +4,20 @@ public static partial class HostConfiguration
 {
     public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
     {
-        builder.AddBusinessLogicInfrastructure().AddExposers().AddDevTools();
+       builder
+            .AddBusinessLogicInfrastructure()
+            .AddExposers()
+            .AddDevTools()
+            .AddCors()
+            .AddMappers()
+            .AddValidators();
 
         return new();
     }
 
     public static ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
     {
+        app.UseCors();
         app.UseExposers().UseDevTools();
 
         return new(app);
